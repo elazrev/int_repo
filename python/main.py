2cdb8b854c420e95ee6430a296d8533ab84f5f39
Elazar Revach <elaz.rev@gmail.com>
2023-07-17 20:17:20 +0300

    Merge changes from origin/main

---
+++ python/main.py
@@ -0,0 +1,12 @@
+def greeting(name, **kwargs):
+    if name:
+        return f'Hello {name}'
+    else:
+        return 'Hello, welcome!'
+
+
+if __name__ == '__main__':
+
+    name = input("What is your name? :")
+    print(greeting(name))
+
